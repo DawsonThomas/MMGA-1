b834871e6d92f2b763464988475767bee984e747
RajatheBeast <pradumn2001@gmail.com>
2016-12-04 22:03:17 -0800

    Defined Mod Class
    got to 7:10 in this video:
    
    https://www.youtube.com/watch?v=9M_I5j25WdQ&list=PLy11IosblXIH-OIKa5yYs-KEA2LCohV8g&index=2

---
+++ src/main/java/jremd/MMGA/Main.java
@@ -0,0 +1,8 @@
+package jremd.MMGA;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
+public class Main {
+
+}
---
+++ src/main/java/jremd/MMGA/Reference.java
@@ -0,0 +1,13 @@
+package jremd.MMGA;
+
+public class Reference {
+
+	public static final String MOD_ID = "MMGA";
+	public static final String NAME = "Make Minecarft Great Again";
+	public static final String VERSION = "0.01-PreAlpha";
+	public static final String ACCEPTED_VERSIONS = "[1.9.4]";
+	
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String SERVER_PROXY_CLASS = "";
+}
+
